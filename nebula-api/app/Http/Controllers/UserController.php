662a44041b6daae72dcf5d8f41f7dd2836188e00
Gabriel Franco <gfranco@_HOSTNAME_>
2022-05-06 13:29:39 +0200

    Validation erros

--- nebula-api/app/Http/Controllers/UserController.php
+++ nebula-api/app/Http/Controllers/UserController.php
@@ -3,26 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    public function registerUser(Request $request) {
-
-       /*  $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => ''
-        ]); */
+    public function registerUser(RegisterRequest $request) {
 
         $name = $request->input('name');
         $email = $request->input('email');
         $password = $request->input('password');
 
-        return response()->json([
-            'status' => 'success',
-            'nom' => $name,
-            'email' => $email,
-            'pass' => $password
-        ]);
+        if($name == 'gabi') {
+            return response()->json([
+                'status' => 'success',
+                'data' => $request->all()
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'data' => $request->all()
+            ]);
+        }
 
     }
 
--- nebula-frontend/src/app/auth/register/register.component.ts
+++ nebula-frontend/src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
+import Swal from 'sweetalert2';  
 
 @Component({
   selector: 'app-register',
@@ -19,9 +20,35 @@ export class RegisterComponent {
 
   registro() {
 
-    this.http.post('http://dev-api.nebula.cat/index.php/api/register', this.datosRegistro).subscribe(res => {
-      console.log(res);
-    });
+    this.http.post('http://dev-api.nebula.cat/index.php/api/register', this.datosRegistro).subscribe(
+      
+      (res: any) => {
+
+      if(res.status == 'success') {
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: '¡Registrado!',
+          text: 'Te has registrado correctamente',
+          showConfirmButton: true,
+          confirmButtonColor: '#7ea966'
+        }).then(function() {
+          window.location.href = "/login";
+        });
+      } else {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: '¡Oopps!',
+          text: 'Has introducido algo mal creo...',
+          showConfirmButton: true,
+          timer: 5000
+        });
+      }
+
+      }
+      
+    );
 
   }
 
